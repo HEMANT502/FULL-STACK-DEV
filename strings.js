94bdfc4116cbc3eabb7a162b374f588da8d3d067
HEMANT502 <hemant.25b01010307@abes.ac.in>
2026-06-23 12:06:14 +0530

    strings

---
+++ strings.js
@@ -0,0 +1,27 @@
+const name="hemant"
+const repoCount=4
+
+//console.log(name+ repoCount+"value")
+
+//console.log(`hello my name is ${name} and my repocount is ${repoCount}`);
+//new and convinient way of concatenating strings .
+
+
+const gamename=new String('hemanthch')
+
+console.log(gamename[0]);
+
+console.log(gamename.length);
+
+console.log(gamename.toUpperCase());
+
+console.log(gamename.toLowerCase());
+
+console.log(gamename.slice(0,5));
+
+console.log(gamename.replace('ch','th'));
+
+console.log(gamename.includes('ch'));
+
+console.log(gamename.split('h'));
+
