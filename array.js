e633821b9814453a61759ed221dc51d076dc42b6
HEMANT502 <hemant.25b01010307@abes.ac.in>
2026-06-23 12:03:29 +0530

    array

---
+++ array.js
@@ -0,0 +1,31 @@
+const myarr=[0,4,5,3,6,7]
+
+
+const myarr2=new Array(1,2,3,4,5,6)
+
+/*console.log(myarr[2]);
+console.log(myarr2[2]);
+
+myarr.push(2)
+
+
+myarr.pop()
+
+myarr.unshift(9)
+myarr.shift()*/
+
+//console.log(myarr.includes(9));
+//console.log(myarr.indexOf(3));
+
+const newarr=myarr.join()
+
+console.log(myarr);
+console.log(newarr);
+console.log(typeof myarr);
+console.log(typeof newarr);
+
+//slice,   splice
+
+
+
+
